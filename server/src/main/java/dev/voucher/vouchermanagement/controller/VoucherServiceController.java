b0536d4778f985c35f6af3f7c195563ed5f6a4cd
Vinh Pham <vinhpham@Vinhs-MacBook-Pro-2.local>
2023-06-28 20:01:57 -0700

    adding server and dependencies

---
+++ server/src/main/java/dev/voucher/vouchermanagement/controller/VoucherServiceController.java
@@ -0,0 +1,9 @@
+package dev.voucher.vouchermanagement.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class VoucherServiceController{
+
+    private VoucherServiceController voucherService;
+}
